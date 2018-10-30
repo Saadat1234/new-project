8c6d7ba6af64062a2ee076992106c8b83721f8d7
Saadat1234 <ahmadkhan.saadat@gmail.com>
2018-10-31 00:20:26 +0100

    created header component to my weather app

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+  render () {
+    return (
+    <div>
+      <h1>Weather App</h1>
+      <p>you can find the cities wether here</p>
+    </div>
+    );
+  }
+}
+
+export default Header
